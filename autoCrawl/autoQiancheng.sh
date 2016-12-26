4a236b4bac50907b6888e283fad3b967cf2ba4d6
VikiQiu <388848@163.com>
2016-12-27 00:34:56 +0800

    auto crawl

---
+++ autoCrawl/autoQiancheng.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+today=$(date +%Y%m%d)
+year=$(date +%Y%m)
+cd /data/github/zhaopin_shucui/qiancheng
+
+scrapy crawl qiancheng -o data/qiancheng$year/[qiancheng]DataMining$today.csv -t csv>>log/log$year/[log]DM$today.txt
+scrapy crawl qiancheng2 -o data/qiancheng$year/[qiancheng]DataAnalysis$today.csv -t csv>>log/log$year/[log]DA$today.txt
